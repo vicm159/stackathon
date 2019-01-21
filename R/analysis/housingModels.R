71501df01e982e5e362d27cfd7e8095e6382d710
Victor Marta <vicm159@gmail.com>
2019-01-21 00:28:54 -0800

    first lm

--- R/analysis/housingModels.R
+++ R/analysis/housingModels.R
@@ -10,6 +10,16 @@ DB <-
 housing <- 
   dbReadTable(DB, Id(schema = "stackathon", name ="housingclean")) 
 
-qplot(soldPrice1, baths, data = housing)
+qplot(baths, soldPrice1/1000, data = housing, ylab = "price in thousands", main = "price and baths")
+qplot(beds, soldPrice1/1000, data = housing, ylab = "price in thousands", main = "price and beds")
+qplot(sqft, soldPrice1/1000, data = housing, ylap = "price in thousands", main = "price and sqft")
 
+qplot(baths, zillowEstimate1/1000, data = housing, ylab = "price in thousands", main = "zillow estimate and baths")
+qplot(beds, zillowEstimate1/1000, data = housing, ylab = "price in thousands", main = "zillow and beds")
+qplot(sqft, zillowEstimate1/1000, data = housing, ylap = "price in thousands", main = "zillow and sqft")
 
+lm_first <- 
+  lm(soldPrice1 ~ beds + baths + sqft + I(sqrt(sqft)) + city, data = housing)
+
+summary(lm_first)
+plot(lm_first)
