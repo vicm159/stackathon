87d6cb1c228100b32e0bda2971a9a7815264de3b
Victor Marta <vicm159@gmail.com>
2019-01-21 14:47:08 -0800

    readme change

--- R/analysis/housingModels.R
+++ R/analysis/housingModels.R
@@ -18,6 +18,11 @@ qplot(baths, zillowEstimate1/1000, data = housing, ylab = "price in thousands", 
 qplot(beds, zillowEstimate1/1000, data = housing, ylab = "price in thousands", main = "zillow and beds")
 qplot(sqft, zillowEstimate1/1000, data = housing, ylap = "price in thousands", main = "zillow and sqft")
 
+test_data <- 
+  housing %>% filter(OnMarket == 0) %>% select(soldPrice1, beds, baths, sqft, city, zillowEstimate1)
+
+
+
 lm_first <- 
   lm(soldPrice1 ~ beds + baths + sqft + I(sqrt(sqft)) + city, data = housing)
 
@@ -26,17 +31,25 @@ plot(lm_first)
 
 test <- 
   filter(housing, OnMarket == 1) %>% 
-  select(beds, baths, sqft, city)
+  select(beds, baths, sqft, city, address1)
 
 lm_predict <-
   predict(lm_first, test)
+
+test$lm_predict <- lm_predict
+
+onMarketTest <- 
+  filter(housing, OnMarket == 0)
+
+lm_predict_onMarket <- 
+  predict(lm_first, onMarketTest)
 
 ggplot(filter(housing, OnMarket == 1), aes(x = zillowEstimate1, y = lm_predict)) +
   geom_point()
 
 ggplot(filter(housing, OnMarket == 1), aes(x = 1:446, y = zillowEstimate1)) +
-  geom_point() +
-  geom_point(aes(y = lm_predict), colour = "red")
+  geom_line() +
+  geom_line(aes(y = lm_predict), colour = "red")
 
 
 
